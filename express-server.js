39ff470ed4bf5043e12976f90130b9de828d492d
nicholas-ewasiuk <ewasiuk.nick@gmail.com>
2021-09-28 17:42:43 +0000

    fixed shortURL

--- express-server.js
+++ express-server.js
@@ -19,7 +19,7 @@ app.get("/urls", (req, res) => {
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[shortURL] };
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("pages/urls_show", templateVars);
 });
 /*
