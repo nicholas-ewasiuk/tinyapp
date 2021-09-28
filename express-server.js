55472e85d92f517c17564e11c95c26d80cdf7fdf
nicholas-ewasiuk <ewasiuk.nick@gmail.com>
2021-09-28 21:37:47 +0000

    added more stuf

--- express-server.js
+++ express-server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const morgan = require("morgan");
+const bodyParser = require("body-parser");
 const app = express();
 const PORT = 8080;
 
@@ -9,6 +10,11 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+function generateRandomString() {
+  return (Math.random() * 1e+18).toString(36).slice(0, 6);
+};
+
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(morgan('dev'));
 
 app.set('view engine', 'ejs');
@@ -18,10 +24,20 @@ app.get("/urls", (req, res) => {
   res.render("pages/urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("pages/urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render("pages/urls_show", templateVars);
 });
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  res.send("Ok");
+});
+
 /*
 app.get("/", (req, res) => {
   let mascots = [
