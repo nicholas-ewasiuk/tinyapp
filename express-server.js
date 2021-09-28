cb4ef6b17e5e858b26fea8b5ba9b81177d077dd1
nicholas-ewasiuk <ewasiuk.nick@gmail.com>
2021-09-28 00:52:55 +0000

    working on it

--- express-server.js
+++ express-server.js
@@ -1,23 +1,51 @@
 const express = require("express");
+const morgan = require("morgan");
 const app = express();
 const PORT = 8080;
+
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
-app.get("/", (req, res) => {
-  res.send("Hello!");
+app.use(morgan('dev'));
+
+app.set('view engine', 'ejs');
+
+app.get("/urls", (req, res) => {
+  const templateVars = { urls: urlDatabase };
+  res.render("pages/urls_index", templateVars);
 });
 
+app.get("/urls/:shortURL", (req, res) => {
+  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[shortURL] };
+  res.render("pages/urls_show", templateVars);
+});
+/*
+app.get("/", (req, res) => {
+  let mascots = [
+    { name: 'Sammy', organization: "DigitalOcean", birth_year: 2012 },
+    { name: 'Tux', organization: "Linux", birth_year: 2013 },
+    { name: 'Moby Dock', organization: "Docker", birth_year: 2013 }
+  ];
+  let tagline = "No programming concept is complete without a cute animal mascot.";
+  
+  res.render('pages/index', {
+    mascots: mascots,i
+    tagline: tagline
+  });
+});
+
+app.get('/about', (req, res) => {
+  res.render('pages/about');
+});
+
+/*
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
-
-app.get("/hello", (req, res) => {
-  res.send("<html><body>Hello <b>World</b></body></html>\n");
-});
+*/
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
