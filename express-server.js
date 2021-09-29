7fe69de50c3fb44f2ac481d2a1beeb3d6d901973
nicholas-ewasiuk <ewasiuk.nick@gmail.com>
2021-09-29 04:51:03 +0000

    added login logout

--- express-server.js
+++ express-server.js
@@ -1,5 +1,6 @@
 const { response } = require("express");
 const express = require("express");
+const cookieParser = require('cookie-parser');
 const morgan = require("morgan");
 const app = express();
 const PORT = 8080;
@@ -15,28 +16,41 @@ function generateRandomString() {
 };
 
 app.use(express.urlencoded({extended: true}));
+app.use(cookieParser());
 app.use(morgan('dev'));
 
 app.set('view engine', 'ejs');
 
 app.get("/urls", (req, res) => {
-  const templateVars = { urls: urlDatabase };
+  const templateVars = { 
+    urls: urlDatabase,
+    username: req.cookies["username"] 
+  };
   res.render("pages/urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("pages/urls_new");
+  const templateVars = { 
+    username: req.cookies["username"] 
+  };
+  res.render("pages/urls_new", templateVars);
 });
 
 app.get("/urls/:shortURL", (req, res) => {
-  const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
-  console.log(templateVars);
+  const templateVars = { 
+    shortURL: req.params.shortURL, 
+    longURL: urlDatabase[req.params.shortURL],
+    username: req.cookies["username"] 
+  };
   res.render("pages/urls_show", templateVars);
 });
 
 app.get("/u/:shortURL", (req, res) => {
+  const templateVars = { 
+    username: req.cookies["username"] 
+  };
   const longURL = urlDatabase[req.params.shortURL];
-  res.redirect(longURL);
+  res.redirect(longURL, templateVars);
 }); 
 
 app.post("/urls", (req, res) => {
@@ -46,6 +60,16 @@ app.post("/urls", (req, res) => {
   urlDatabase[shortURL] = longURL;
 
   res.redirect(`/urls/${shortURL}`);
+});
+
+app.post("/login", (req, res) => {
+  res.cookie('username', req.body.username);
+  res.redirect('/urls');
+});
+
+app.post("/logout/", (req, res) => {
+  res.clearCookie('username');
+  res.redirect('/urls');
 });
 
 app.post("/urls/:shortURL/delete", (req, res) => {
