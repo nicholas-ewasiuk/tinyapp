5e6801b3d53267046230cb0bd594fb7dcd780380
nicholas-ewasiuk <ewasiuk.nick@gmail.com>
2021-09-30 21:06:07 +0000

    added user schema

--- models/user.js
+++ models/user.js
@@ -4,39 +4,11 @@ const Schema = mongoose.Schema;
 
 const UserSchema = new Schema(
   {
+    userId: {type: String, required: true, maxLength: 100},
     email: {type: String, required: true, maxLength: 100},
-    family_name: {type: String, required: true, maxLength: 100},
-    date_of_birth: {type: Date},
-    date_of_death: {type: Date},
+    password: {type: String, required: true, maxLength: 100},
   }
 );
-
-// Virtual for author's full name
-UserSchema
-.virtual('name')
-.get(function () {
-  return this.family_name + ', ' + this.first_name;
-});
-
-// Virtual for author's lifespan
-UserSchema.virtual('lifespan').get(function() {
-  var lifetime_string = '';
-  if (this.date_of_birth) {
-    lifetime_string = DateTime.fromJSDate(this.date_of_birth).toLocaleString(DateTime.DATE_MED);
-  }
-  lifetime_string += ' - ';
-  if (this.date_of_death) {
-    lifetime_string += DateTime.fromJSDate(this.date_of_death).toLocaleString(DateTime.DATE_MED)
-  }
-  return lifetime_string;
-});
-
-// Virtual for author's URL
-UserSchema
-.virtual('url')
-.get(function () {
-  return '/catalog/author/' + this._id;
-});
 
 //Export model
 module.exports = mongoose.model('Author', UserSchema);
