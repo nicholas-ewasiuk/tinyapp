22a8f344a5a38a45d41407fb0e82e0c97242c039
nicholas-ewasiuk <ewasiuk.nick@gmail.com>
2021-09-30 20:59:06 +0000

    using MDN tutorial docs to incorporate MongoDB for user info

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ const express = require("express");
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const morgan = require("morgan");
+const mongoose = require("mongoose");
+const mongoDB = 'mongodb+srv://dbUser:arrayfunctionnew@cluster0.x8tjj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 const indexRouter = require('./routes/index');
 const loginRouter = require('./routes/login');
