2847a73007e904ad7d80caffb9d73361632afc04
nicholas-ewasiuk <ewasiuk.nick@gmail.com>
2021-09-30 19:32:20 +0000

    setting up routes

--- app.js
+++ app.js
@@ -3,6 +3,12 @@ const express = require("express");
 const cookieParser = require('cookie-parser');
 const morgan = require("morgan");
 
+const indexRouter = require('./routes/index');
+const loginRouter = require('./routes/login');
+const newRouter = require('./routes/new');
+const registerRouter = require('./routes/register');
+const showRouter = require('./routes/show');
+
 const app = express();
 
 app.set('views', path.join(_dirname, 'views'));
@@ -11,6 +17,13 @@ app.set('view engine', 'ejs');
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
 app.use(morgan('dev'));
-app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/login', loginRouter);
+app.use('/new', newRouter);
+app.use('/register', registerRouter);
+app.use('/show', showRouter);
+
+
 
 module.exports = app;
