170c8675c549c0789ee04f26661defae73fd21d2
nicholas-ewasiuk <ewasiuk.nick@gmail.com>
2021-09-30 21:37:37 +0000

    getting rid of db, shouldve branched it

--- app.js
+++ app.js
@@ -3,11 +3,6 @@ const express = require("express");
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const morgan = require("morgan");
-const mongoose = require("mongoose");
-const mongoDB = 'mongodb+srv://dbUser:arrayfunctionnew@cluster0.x8tjj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
-mongoose.connect(mongoDB, { useNewUrlParser: true , useUnifiedTopology: true});
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 const indexRouter = require('./routes/index');
 const loginRouter = require('./routes/login');
@@ -28,6 +23,5 @@ app.use('/login', loginRouter);
 app.use('/new', newRouter);
 app.use('/register', registerRouter);
 app.use('/show', showRouter);
-
 
 module.exports = app;
